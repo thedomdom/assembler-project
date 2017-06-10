7b1e849548a3d601d70e66675a53fe2d225d3915
D065057 <thedomdom96@gmail.com>
2017-06-10 16:22:09 +0200

    Fixed LEDs

--- eierkocher.asm
+++ eierkocher.asm
@@ -1,33 +1,33 @@
-; Eierkocher 
+; Eierkocher
 ; (Projekt Systemnahe Programmierung)
-; Tom Bendrath, Dominik Wunderlich, Enrico Kaack und Torben Krieger 
+; Tom Bendrath, Dominik Wunderlich, Enrico Kaack und Torben Krieger
 
 CSEG AT 0H
-LJMP init
+LJMP start
 ORG 100H
 
 ; --------------------------
-; init
+; start
 ; --------------------------
-init:
-; Variablen
-;-----------------
+start:
 ; P0.0 - Taster
 ; P0.1 - Hupe
 ; P2 - Temperatur (Wunschtemp)
 ; R0 - Temperatursensor
 ; R1 - Wassermenge
 ; P1 - Display
-	;Aliases
+
 	; input
 	IN0 equ 20H
-	temp_high equ IN0.0
-	button equ IN0.1
+	temp_high_in equ IN0.0
+	button_in equ IN0.1
+	
 	; output
 	OUT0 equ 21H
-	horn_led equ OUT0.0
-	heating_led equ OUT0.1
-	active_led equ OUT0.2
+	active_led_out equ OUT0.0
+	horn_out equ OUT0.1
+	heating_out equ OUT0.2
+	
 	; IO tempsensor
 	tempsensor_dq equ P1.0
 	tempsensor_clk equ P1.1
@@ -35,19 +35,29 @@ init:
 	tempsensor_com equ P1.3
 	tempsensor_low equ P1.4
 	tempsensor_high equ P1.5
+
 	; internal
 	temp_max equ 22H
+	active equ R0
 	timer equ R1
 	timer_max equ R2
-	active equ R0
-	
+
+; Initialize
 	MOV IN0, #00H
 	MOV OUT0, #00H
+	; Initialize LEDs
+	SETB active_led_out
+	SETB horn_out
+	SETB heating_out
+
+	; Initialize internals
 	MOV active, #00H
 	MOV timer, #00H
-	
+
 	MOV temp_max, #100
 	MOV timer_max, #100 ; TODO set right time here
+
+	; Initialize temperature sensor
 	CALL resetsensor
 	CALL sensortick
 	CALL writemaxtemp
@@ -57,32 +67,52 @@ read:
 	; read button
 	MOV c, P0.1
 	CPL c
-	MOV button, c
+	MOV button_in, c
 	; read temp_high
 	MOV c, tempsensor_high
-	MOV temp_high, c
+	MOV temp_high_in, c
 
 	; If active --> checktemp
-	;CJNE active, #00H, checktemp
+	CJNE active, #00H, checktemp
 	; Else if button pressed --> setactive
-	JB button, setactive
+	JB button_in, setactive
 	; Else --> read
 	JMP read
 
 setactive:
 	;active = 1111 1111
 	MOV active, #0FFh
+	; activate active LED
+	CLR active_led_out
 	; --> checktemp
 	JMP checktemp
 
 checktemp:
+	; if temperature > 100 deg --> timer
+	JB temp_high_in, checktimer
+	; else --> heat
+	JMP heat
 
 heat:
+	; Activate heating
+	CLR heating_out
+	; --> output
+	JMP output
 
 checktimer:
+	; Deactivate Heating
+	SETB heating_out
+	; if timer started --> output
+	CJNE timer, #00H, output
+	; else --> starttimer
+	MOV timer, #0FFh
 
 output:
-LJMP read
+
+;	Move output register
+	MOV P2, OUT0
+
+	LJMP read
 
 
 ;----------------------------------------------------
